d99f340a49e873f54f946c3723514c19a9454178
MurkyMilk <s4viour@gmail.com>
2016-10-29 14:33:39 +0200

    refactor - code more readable

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ realname = "Mlecko"  # nazwa
 
 ######################################
 
-def init(sock):
-    bufor = ""
+def worker(sock):
+    whole_message = ""
     end = 0
     nickname = ""
     password = ""
@@ -36,60 +36,60 @@ def init(sock):
     lkolor = color
     lbold = bold
     lemoty = emoty
-    while 1:
-        b1 = ""
-        b1 = sock.recv(1024)
-        if b1 == "": return
-        bufor += b1.decode()
-        if not bufor.find("NICK") == -1:
-            nickname = findall("NICK (.*?)(\r|\n)", bufor)[0][0]
-            if nickname[0] == '~':
-                break
-        if not bufor.find("PASS") == -1:
-            password = findall("PASS (.*?)(\r|\n)", bufor)[0][0]
-        if (password != "") and (nickname != ""):
-            print("wtf")
-            break
+
+    while ((password == "") or (nickname == "")):
+        received_chunk = sock.recv(1024)
+        if received_chunk == "": return
+        whole_message += received_chunk.decode()
+        if not whole_message.find("NICK") == -1:
+            nickname = extract_nick(whole_message)
+        if not whole_message.find("PASS") == -1:
+            password = extract_password(whole_message)
 
     send_welcome_messages(lbold, lkolor, sock)
-    if not TUNEL_PASS == "":
+
+    if TUNEL_PASS:
         password = password.split(':')
-        if not len(password) == 2:
-            return wrong_password_parsing(sock)
-        if not password[0] == TUNEL_PASS:
-            return inform_wrong_tunnel_password(sock)
+        if len(password) != 2:
+            wrong_password_parsing(sock)
+            sock.close()
+            return
+        if password[0] != TUNEL_PASS:
+            inform_wrong_tunnel_password(sock)
+            sock.close()
+            return
         password = password[1]
     sys.stdout.write("nick: %s\n" % nickname)
     try:
 
         UOkey = authorization(nickname, password)
-
-    # sock.send((":fake.host 666 nik : 10[Tunel] \r\n"
-    #             ":fake.host 2012 " + nickname + " : 10[Tunel] Twoj UOkey: " + UOkey + "\r\n"))
-    #  sock.send((":Tunel!fake@fake.fake PRIVMSG fake :VERSION\r\n"))
-    except:
-        print(sys.exc_info()[0])
-        get_date_string()
-        my_err = "[%3d] ERROR: sprawdz swoje polaczenie sieciowe z internetem, poprawnosc wprowadzonego hasla i nicka lub dostepnosc serwerow onet.pl (zmiany autoryzacji)\r\n" % ID
-        sys.stdout.write(my_err)
-        sock.send(str.encode(my_err))
+    except Exception as e:
+        print(e)
+        sock.send(str.encode("ERROR: sprawdz swoje polaczenie sieciowe z internetem, poprawnosc wprowadzonego hasla i nicka lub dostepnosc serwerow onet.pl"))
         sock.close()
         return
+
     sockets = [sock]
     onet = connect_to_onet(UOkey, nickname, sock, sockets)
     mainLoop(UOkey, encode, end, lbold, lemoty, lkolor, nickname, onet, sock, sockets)
 
 
+def extract_password(whole_message):
+    password = findall("PASS (.*?)(\r|\n)", whole_message)[0][0]
+    return password
+
+
+def extract_nick(whole_message):
+    nickname = findall("NICK (.*?)(\r|\n)", whole_message)[0][0]
+    return nickname
+
+
 def wrong_password_parsing(sock):
     send(sock, "uzywaj: /server host port haslo_do_tunel:haslo_do_nick\r\n")
-    sock.close()
-    return
 
 
 def inform_wrong_tunnel_password(sock):
     send(sock, "zle haslo do tunela\r\n")
-    sock.close()
-    return
 
 
 def connect_to_onet(UOkey, nickname, sock, sockets):
@@ -101,7 +101,6 @@ def connect_to_onet(UOkey, nickname, sock, sockets):
     onet.send(str.encode("AUTHKEY\r\n"))
     onet.send(str.encode("NICK " + nickname + "\r\n"))
     onet.send(str.encode("USER * " + UOkey + " czat-app.onet.pl :" + nickname + "\r\n"))
-    # onet.send("USER * %s czat-app.onet.pl :%s\r\n" % (UOkey, realname))
     authkey = auth(findall(":.*?801.*?:(.*?)\r", onet.recv(1024).decode("utf-8", "ignore"))[0])
     onet.send(str.encode("AUTHKEY " + authkey + "\r\n"))
     return onet
@@ -175,5 +174,5 @@ createSocketConnection(s, port, local_ip)
 s.listen(5)
 while 1:
     c, cinfo = s.accept()
-    threading.Thread(target=init, args=[c]).start()
+    threading.Thread(target=worker, args=[c]).start()
 s.close()
